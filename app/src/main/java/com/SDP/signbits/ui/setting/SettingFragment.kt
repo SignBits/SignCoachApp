960a98a51ee7f91efc68ab89bfdc8584e2ad20f3
Raman Goyal <goyalraman17@gmail.com>
2020-02-20 13:16:05 +0000

    Settings changed

--- app/src/main/java/com/SDP/signbits/ui/setting/SettingFragment.kt
+++ app/src/main/java/com/SDP/signbits/ui/setting/SettingFragment.kt
@@ -6,8 +6,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.FragmentManager
 
 import com.SDP.signbits.R
+import com.SDP.signbits.ui.quiz.QuizViewModel
 
 class SettingFragment : Fragment() {
 
@@ -15,19 +18,45 @@ class SettingFragment : Fragment() {
         fun newInstance() = SettingFragment()
     }
 
-    private lateinit var viewModel: SettingViewModel
+    private lateinit var settingsviewModel: SettingViewModel
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_setting, container, false)
+        settingsviewModel =
+            ViewModelProviders.of(this).get(SettingViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_setting, container, false)
+        // These are the buttons in the quiz fragment view
+        val textupdates : TextView = root.findViewById(R.id.textView2)
+        val textsearchbot : TextView = root.findViewById(R.id.textView3)
+        val textcontactus : TextView = root.findViewById(R.id.textView4)
+        val textterms : TextView = root.findViewById(R.id.textView5)
+        val textcache : TextView = root.findViewById(R.id.textView6)
+        val textRpi : TextView = root.findViewById(R.id.textView7)
+//        textupdates.setOnClickListener{
+//            convertToAnotherFragment(SettingTerms.newInstance())
+//        }
+
+        return root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(SettingViewModel::class.java)
+        settingsviewModel = ViewModelProviders.of(this).get(SettingViewModel::class.java)
         // TODO: Use the ViewModel
-    }
 
+    }
+    /**
+     * This is the method to convert to another fragment.
+     *
+     * @param fragment This is the fragment of the destination. Need to be a fragment object instead
+     *        of Fragment Id etc
+     */
+    private fun convertToAnotherFragment(fragment: Fragment){
+        val fragmentManger : FragmentManager = requireFragmentManager()
+        fragmentManger.beginTransaction().replace(this.id, fragment).commit()
+
+    }
 }
