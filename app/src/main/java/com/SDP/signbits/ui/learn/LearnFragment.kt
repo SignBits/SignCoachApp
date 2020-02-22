58789d7da1d09f07ab5148aab6a15256ba2b65b2
zhichengg <s1762811@sms.ed.ac.uk>
2020-02-22 20:20:19 +0000

    Fix some bugs

--- app/src/main/java/com/SDP/signbits/ui/learn/LearnFragment.kt
+++ app/src/main/java/com/SDP/signbits/ui/learn/LearnFragment.kt
@@ -4,15 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.SDP.signbits.R
+import android.graphics.Color;
+import com.SDP.signbits.ui.quizCharToFinger.QuizCharToFinger
 
 class LearnFragment : Fragment() {
 
     private lateinit var learnViewModel: LearnViewModel
+
+    private var current_char = 0
+    val char_array: IntArray = intArrayOf(R.mipmap.ic_letter_b)
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,10 +29,43 @@ class LearnFragment : Fragment() {
         learnViewModel =
             ViewModelProviders.of(this).get(LearnViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_learn, container, false)
-        val textView: TextView = root.findViewById(R.id.text_learn)
+        //val textView: TextView = root.findViewById(R.id.text_learn)
         learnViewModel.text.observe(this, Observer {
-            textView.text = it
+            //textView.text = it
         })
-        return root
+
+        val button_previous : Button = root.findViewById(R.id.learn_previous)
+        val button_next : Button = root.findViewById(R.id.learn_next)
+        val button_finger :Button = root.findViewById(R.id.learn_fingerspell)
+
+        val image : ImageView = root.findViewById(R.id.learn_image)
+        image.setImageResource(char_array[current_char])
+        button_previous.setOnClickListener{
+            if(current_char>0)
+                image.setImageResource(char_array[--current_char])
+        }
+
+        button_next.setOnClickListener{
+            if(current_char<25)
+                image.setImageResource(char_array[++current_char])
+
+        }
+
+        var iscon =true
+        button_finger.setOnClickListener{
+            if(iscon){
+                FingerSpell()
+                button_finger.setTextColor(Color.RED)
+            }
+
+
+        }
+
+    return root
+
+    }
+
+    private fun FingerSpell(){
+        return
     }
 }
--- app/src/main/java/com/SDP/signbits/ui/quizCharToFinger/QuizCharToFinger.kt
+++ app/src/main/java/com/SDP/signbits/ui/quizCharToFinger/QuizCharToFinger.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Observer
@@ -22,16 +23,48 @@ class QuizCharToFinger : Fragment() {
 
     private lateinit var viewModel: QuizCharToFingerViewModel
 
+    private var current_char = 0
+    val char_array: IntArray = intArrayOf(R.mipmap.ic_letter_b)
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         viewModel = ViewModelProviders.of(this).get(QuizCharToFingerViewModel::class.java)
-        val root = inflater.inflate(R.layout.quiz_char_to_finger_fragment, container, false)
+        val root = inflater.inflate(R.layout.quiz_char_to_finger_fragment_new, container, false)
 
         val button : Button = root.findViewById(R.id.Button)
         button.setOnClickListener{
             backToQuiz()
+        }
+
+        val button_right : Button = root.findViewById(R.id.btn_quiz_right)
+        val button_wrong : Button = root.findViewById(R.id.btn_quiz_wrong)
+
+        var text_complete : TextView = root.findViewById(R.id.quiz_complete)
+
+        val image : ImageView = root.findViewById(R.id.quiz_image)
+        image.setImageResource(char_array[current_char])
+        button_wrong.setOnClickListener{
+
+        }
+
+        button_right.setOnClickListener{
+            if(current_char<25) {
+                image.setImageResource(char_array[++current_char])
+                text_complete.text = current_char.toString() + " of 26 tasks are completed"
+            }else
+                text_complete.text = "26 of 26 tasks are completed"
+        }
+
+        button_wrong.setOnClickListener{
+
+        }
+
+        val button_next : Button = root.findViewById(R.id.button6)
+        button_next.setOnClickListener{
+            if(current_char<25)
+                image.setImageResource(char_array[++current_char])
         }
 
         return root
@@ -43,8 +76,7 @@ class QuizCharToFinger : Fragment() {
      */
     private fun backToQuiz() {
         val fragmentManager : FragmentManager = requireFragmentManager()
-        fragmentManager.beginTransaction().remove(this).commit()
-        fragmentManager.popBackStack()
+        fragmentManager.beginTransaction().replace(this.id, QuizFragment()).commit()
     }
 
 
