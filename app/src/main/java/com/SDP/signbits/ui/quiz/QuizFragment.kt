f6c8ceaf6a5fe1bc082107df8cee2f473574d771
vioqiao <32181206+vioqiao@users.noreply.github.com>
2020-03-10 03:22:59 +0000

    update

--- app/src/main/java/com/SDP/signbits/ui/quiz/QuizFragment.kt
+++ app/src/main/java/com/SDP/signbits/ui/quiz/QuizFragment.kt
@@ -5,16 +5,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.ViewModelProviders
-import com.SDP.signbits.MainActivity
 import com.SDP.signbits.R
 import com.SDP.signbits.ui.quizCharToFinger.QuizCharToFinger
 import com.SDP.signbits.ui.quizFingerToChar.QuizFingerToChar
-import com.SDP.signbits.TextProgressBar
 
 
 class QuizFragment : Fragment() {
@@ -35,7 +34,7 @@ class QuizFragment : Fragment() {
         val buttonF2C : CardView = root.findViewById(R.id.cardF2C)
 
         val pref : SharedPreferences = requireContext().getSharedPreferences("LearningProgress",0)
-        val pbC2F : TextProgressBar = root.findViewById(R.id.progressBar3)
+        val pbC2F : ProgressBar = root.findViewById(R.id.progressBar3)
         pbC2F.max = pref.getInt("C2FNumber", 1)
         if (pbC2F.max == 0) pbC2F.max =1
         pbC2F.progress = pref.getInt("C2FCorrect", -1)
@@ -45,7 +44,10 @@ class QuizFragment : Fragment() {
         textView.text = "You have done ${pbC2F.progress} Character to Fingerspell " +
                 "quizzes with accuracy of ${pbC2F.progress * 100 /pbC2F.max}"
 
-        val pbF2C : TextProgressBar = root.findViewById(R.id.progressBar4)
+        val tv_1 : TextView = root.findViewById(R.id.tv_1)
+        tv_1.text = "${pbC2F.progress * 100 /pbC2F.max}%"
+
+        val pbF2C : ProgressBar = root.findViewById(R.id.progressBar4)
         pbF2C.max = pref.getInt("F2CNumber", 1)
         if (pbF2C.max == 0) pbF2C.max = 1
         pbF2C.progress = pref.getInt("F2CCorrect", -1)
@@ -53,6 +55,9 @@ class QuizFragment : Fragment() {
         val textView2 : TextView = root.findViewById(R.id.textView14)
         textView2.text = "You have done ${pbF2C.progress} Fingerspell to Character quizzes with " +
                 "accuracy of ${pbF2C.progress*100/pbF2C.max}%"
+
+        val tv_2 : TextView = root.findViewById(R.id.tv_2)
+        tv_2.text = "${pbF2C.progress * 100 /pbF2C.max}%"
 
         buttonC2F.setOnClickListener{
             convertToAnotherFragment(QuizCharToFinger.newInstance())
@@ -67,7 +72,7 @@ class QuizFragment : Fragment() {
     override fun onResume() {
         super.onResume()
         val pref : SharedPreferences = requireContext().getSharedPreferences("LearningProgress",0)
-        val pbC2F : TextProgressBar = requireActivity().findViewById(R.id.progressBar3)
+        val pbC2F : ProgressBar = requireActivity().findViewById(R.id.progressBar3)
         pbC2F.max = pref.getInt("C2FNumber", 1)
         if (pbC2F.max == 0) pbC2F.max =1
         pbC2F.progress = pref.getInt("C2FCorrect", -1)
@@ -76,7 +81,10 @@ class QuizFragment : Fragment() {
         textView.text = "You have done ${pbC2F.progress} Character to Fingerspell " +
                 "quizzes with accuracy of ${pbC2F.progress * 100 /pbC2F.max}"
 
-        val pbF2C : TextProgressBar = requireActivity().findViewById(R.id.progressBar4)
+        val tv_1 : TextView = requireActivity().findViewById(R.id.tv_1)
+        tv_1.text = "${pbC2F.progress * 100 /pbC2F.max}%"
+
+        val pbF2C : ProgressBar = requireActivity().findViewById(R.id.progressBar4)
         pbF2C.max = pref.getInt("F2CNumber", 1)
         if (pbF2C.max == 0) pbF2C.max = 1
         pbF2C.progress = pref.getInt("F2CCorrect", -1)
@@ -84,6 +92,9 @@ class QuizFragment : Fragment() {
         val textView2 : TextView = requireActivity().findViewById(R.id.textView14)
         textView2.text = "You have done ${pbF2C.progress} Fingerspell to Character quizzes with " +
                 "accuracy of ${pbF2C.progress*100/pbF2C.max}%"
+
+        val tv_2 : TextView = requireActivity().findViewById(R.id.tv_2)
+        tv_2.text = "${pbF2C.progress * 100 /pbF2C.max}%"
     }
 
     /**
